55bdae6e1b369e8f679ad386ba263c1cc18a9a96
karanDevX11 <bistakaran89@gmail.com>
2024-12-23 14:22:33 +0545

    Update app password after changing password

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'karandevx22@gmail.com'
-EMAIL_HOST_PASSWORD = 'cctl ixkz oiut muho'  #app password
+EMAIL_HOST_PASSWORD = 'zzil mqwt jcvk dwzy'  #app password
