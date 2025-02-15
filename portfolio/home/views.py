6418ed12e7c4386be7653a35802686867b6f5426
karanDevX11 <bistakaran89@gmail.com>
2025-02-15 21:13:59 +0545

    Add send email logic for view

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.core.mail import send_mail
+from django.conf import settings
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -12,4 +15,26 @@ class HomeView(TemplateView):
 
 class RecruiterView(TemplateView):
     template_name = "home/recruiter.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title': 'Recruiter'})
+    
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        # Send email
+        try:
+            send_mail(
+                subject= f"New message from {name}",
+                message= f"Name: {name}\nEmail: {email}\nMessage: {message}",
+                from_email= settings.EMAIL_HOST_USER,
+                recipient_list= [settings.EMAIL_HOST_USER],
+                fail_silently=False,
+            )
+            return JsonResponse({'success': True, 'message': 'Your message has been sent successfully!'})
+        except Exception as e:
+            return JsonResponse({'success': False, 'message': 'Failed to send message. Please try again later.'})
+        
 
