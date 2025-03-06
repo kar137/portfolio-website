070d6626ad212c3ca804fd557245673728d9f2a1
karanDevX11 <bistakaran89@gmail.com>
2025-03-06 11:28:13 +0545

    Add views and styling for adventurer page

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, BrowseView, RecruiterView, DeveloperView, StalkerView
+from .views import HomeView, BrowseView, RecruiterView, DeveloperView, StalkerView, AdventurerView
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name="home"),
