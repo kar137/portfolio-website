7a705a49cd85e1fd1895a2f2a8b1e968b770c721
karanDevX11 <bistakaran89@gmail.com>
2025-03-18 13:06:43 +0545

    Add model for projects

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import FavoriteMovies
+from .models import FavoriteMovies, Projects
 
 # Register your models here.
 
 admin.site.register(FavoriteMovies)
+admin.site.register(Projects)
--- home/models.py
+++ home/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Projects(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+    image = models.ImageField(upload_to= "images/projects/")
+    srclink = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.name}"
+
 class FavoriteMovies(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=200)
@@ -9,5 +18,5 @@ class FavoriteMovies(models.Model):
     rating = models.IntegerField(default=1)
 
     def __str__(self):
-        return f"{self.title} {self.rating}"
+        return f"{self.title} - {self.rating}"
 
