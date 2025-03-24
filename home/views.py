5943d3b73b47abd4ebc6e799a2e4aed6c2ede016
karanDevX11 <bistakaran89@gmail.com>
2025-03-24 18:34:59 +0545

    Update css for projects

--- home/views.py
+++ home/views.py
@@ -52,7 +52,7 @@ class DeveloperView(ListView):
     context_object_name = "projects"
 
     def get_queryset(self):
-        return Projects.objects.all().order_by('-date')
+        return Projects.objects.all().order_by('date')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
